3f6873261478d23e7f174c0acc57ca2de221eb9f
Bao-Linh Le Mai <mailebaolinh@gmail.com>
2025-04-02 12:55:35 +0100

    Adding new method of imputation (for dataset improvement, Task 2 - subtask 1); Add removing outlier method to notebooks

--- modules/cleansing.py
+++ modules/cleansing.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer, KNNImputer
+from sklearn.ensemble import RandomForestClassifier
 
 def remove_outliers(df: pd.DataFrame, column) -> pd.DataFrame:
     q1 = df[column].quantile(0.25)
@@ -7,7 +10,11 @@ def remove_outliers(df: pd.DataFrame, column) -> pd.DataFrame:
     iqr = q3 - q1
     lower_bound = q1 - 1.5 * iqr
     upper_bound = q3 + 1.5 * iqr
+    print(f'Q1: {q1}, Q3: {q3}')
+    print(f'Lower bound: {lower_bound}, Upper bound: {upper_bound}')
+    print(f'Number of rows before removing outliers: {df.shape[0]}')
     df = df[(df[column] > lower_bound) & (df[column] < upper_bound)]
+    print(f'Number of rows after removing outliers: {df.shape[0]}')
     return df
 
 def remove_invalid_values(df: pd.DataFrame) -> pd.DataFrame:
@@ -27,9 +34,23 @@ def mapping_bool_values(df: pd.DataFrame, excluded_columns: list) -> pd.DataFram
                 df[column] = df[column].replace({'1.0': '1', '2.0': '0'})
             df[column] = df[column].astype(np.float64)
             
-    # df['PREGNANT'] = df['PREGNANT'].fillna(0.0)
+    df['PREGNANT'] = df['PREGNANT'].fillna(0.0)
     
     return df
 
 def remove_columns(df: pd.DataFrame, columns: list) -> pd.DataFrame:
     return df.drop(columns, axis=1)
+
+def impute_missing_values(df: pd.DataFrame, excluded_columns: list) -> pd.DataFrame:
+    tmp_df = df.loc[:, [x for x in df.columns if x not in excluded_columns]] 
+    # print(tmp_df)
+    imputer = KNNImputer(n_neighbors=5)
+    imputed_values = imputer.fit_transform(tmp_df)
+    # imp = IterativeImputer(max_iter=10, random_state=0, initial_strategy='most_frequent')
+    # imputed_values = imp.fit_transform(tmp_df)
+    # print(type(imputed_values))
+
+    df.loc[:, [x for x in df.columns if x not in excluded_columns]] = imputed_values
+    df[[x for x in df.columns if x not in excluded_columns]] = df[[x for x in df.columns if x not in excluded_columns]].apply(np.ceil)
+
+    return df
